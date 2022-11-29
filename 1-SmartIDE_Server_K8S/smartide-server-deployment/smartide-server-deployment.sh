2df8badad3ee54dd4eabee5fccc219a14bf03fb4
leebchi <chileeb@163.com>
2022-11-29 17:35:15 +0800

    server manifests

---
+++ 1-SmartIDE_Server_K8S/smartide-server-deployment/smartide-server-deployment.sh
@@ -0,0 +1,7 @@
+kubectl apply -f k8s/gva-server-configmap.yaml
+kubectl apply -f k8s/gva-server-deployment.yaml
+kubectl apply -f k8s/gva-web-configmap.yaml
+kubectl apply -f k8s/gva-web-deploymemt.yaml
+kubectl apply -f k8s/myadmin-deployment.yaml
+kubectl apply -f k8s/mysql-deployment.yaml
+kubectl apply -f k8s/redis-deployment.yaml
